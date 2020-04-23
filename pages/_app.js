44f14ab4feafe7eaf187ba4946cb37aec1e6e03c
karimmhamdi <contact@nourlabs.fr>
2020-04-23 16:31:14 +0200

    correction Navbar.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,33 +2,18 @@ import React from "react";
 import App, {Container as NextContainer} from "next/app";
 import Head from "next/head";
 import {DefaultSeo} from "next-seo";
-import Navbar from "../components/Layouts/Navbar";
 
 class MyApp extends App {
     static async getInitialProps({Component, ctx}) {
-        let pageProps = {};
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx);
+        return {
+            pageProps: Component.getInitialProps
+                ? await Component.getInitialProps(ctx)
+                : {}
         }
-        if (ctx.req && ctx.req.session.passport) {
-            pageProps.user = ctx.req.session.passport.user;
-        }
-        return {pageProps};
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: props.pageProps.user
-        };
     }
 
     render() {
-        const {Component, pageProps} = this.props;
-        const props = {
-            ...pageProps,
-            user: this.state.user,
-        };
+        const {Component, pageProps, store} = this.props;
 
         return (
             <NextContainer>
@@ -44,8 +29,7 @@ class MyApp extends App {
                 <Head>
                     <link rel="stylesheet" type="text/css" href={`/styles/style.css`}/>
                 </Head>
-                <Navbar user={this.state['user']}/>
-                <Component {...props}/>
+                <Component {...pageProps}/>
 
             </NextContainer>
         );
